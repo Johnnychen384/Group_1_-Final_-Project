ea0b9e6a70234ce60c97a3954e2bb9d4bd01f791
unknown <therryhicks@gmail.com>
2022-06-20 16:33:26 -0400

    js

---
+++ js/taskManager.js
@@ -0,0 +1,58 @@
+class TaskMangaer{
+    tasks;
+}
+constructor(tasks) {
+    this.task = task ;
+
+}
+const task1 = {
+    id: 1,
+    name: 'start a load of laundry',
+    description: 'wash, dry, and fold clothes',
+    assignedTo: 'Johnny',
+    dueDate: '2022-06-25',
+    status: 'TODO'
+};
+
+const task2 = {
+    id:2,
+    name:'clean the family car',
+    description:' wash and vaccum the family car',
+    assignedTo: 'Eskinder',
+    dueDate: '2022-06-25',
+    status: 'TODO'
+};
+
+const task3 ={
+    id: 3,
+    name:'pick up groceries',
+    description: 'purchase items on grocery list',
+    assignedTo: 'Amanda',
+    dueDate: '2022-06-25',
+    status: 'TODO'
+};
+
+const task4 = {
+    id:4,
+    name: 'meal prep',
+    description: 'prepare and put away meals for the week',
+    assignedTo: 'Richard',
+    dueDate: '2022-06-25',
+    status: 'TODO'
+};
+
+const task5 = {
+    id:5,
+    name:'clean the pool',
+    description: 'adjust the waters pH and remove debris',
+    assignedTo: 'Therry',
+    dueDate: '2022-06-25',
+    status:'TODO'
+};
+class addTask {
+    Id;
+}
+constructor(Id) {
+    this.currentId = currentId;
+}
+function 
